806ced654bd6c04998a91ffbf38b6d646e3b5bf1
bhomitb <bhomitb@yahoo.com>
2017-05-05 15:12:26 +0530

    Upload Images feature added

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var multer  = require('multer');
-var upload = multer({ dest: 'public/uploads/' });
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/upload/images/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+ 
+var upload = multer({ storage: storage })
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 /* GET home page. */
 router.get('/', function(req, res, next) {
 res.render('fileupload');
 });
 
+router.post('/', upload.any(), function(req, res, next){
+    console.log(req.files);
+ res.send(req.files);
+});
 module.exports = router;
